351f03cc0d85045d570199b472e6be7ba2cb1a65
tazwar23 <atazwar@myseneca.ca>
2024-02-04 17:59:21 -0500

    Initialized main function in app.js

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {useAssets} from 'expo-asset'
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
@@ -18,3 +20,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function Main(){
+  const [assets] = useAssets(
+  
+  );
+  if(!assets){
+    return <Text>Loading ..</Text>;
+  }
+  return <App/>;
+}
