358ae2cbb3becb55651b158ced9859dbd93037e7
tazwar23 <atazwar@myseneca.ca>
2024-02-04 16:58:57 -0500

    firebase.js initialized

---
+++ ChatApp/firebase.js
@@ -0,0 +1,33 @@
+//firebase.js implements all the database setup for ChatFusion
+//
+import { initializeApp } from "firebase";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaXciTDYfYFiSVoJmPOzkkY1Vj8AKIV9A",
+  authDomain: "chatfusion-9104b.firebaseapp.com",
+  projectId: "chatfusion-9104b",
+  storageBucket: "chatfusion-9104b.appspot.com",
+  messagingSenderId: "446430013858",
+  appId: "1:446430013858:web:c288dc23f775a764ee2afb",
+  measurementId: "G-175DHFPCR6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+
+//Firebase util functions
+
+
+export function signIn(email,password){
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+export function signUp(email,password){
+  return createUserWithEmailAndPassword(auth, email,password)
+}
