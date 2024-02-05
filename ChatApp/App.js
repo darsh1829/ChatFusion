6dc8bac8a785a0521adac7b4bc031782ea138308
tazwar23 <atazwar@myseneca.ca>
2024-02-04 19:28:44 -0500

    Trying to work app.js

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect, LogBox} from 'react';
 import {useAssets} from 'expo-asset'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
+LogBox.ignores([
+  "Setting a timer",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release.",
+])
 function App() {
+  const [currUser, setCurrUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  //
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false)
+      if(user){
+        setCurrUser(user);
+      }
+    });
+    return() => unsubscribe();
+  }, [])
+  if(loading){
+    return <Text>Loading....</Text>
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>{JSON.stringify(currUser)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -26,7 +47,7 @@ function Main(){
     require("./assets/icon-square.png"),
     require("./assets/chatbg.png"),
     require("./assets/user-icon.png"),
-    require("./assets/welcome-img.png")
+    require("./assets/welcome.png")
   );
   if(!assets){
     return <Text>Loading ..</Text>;
